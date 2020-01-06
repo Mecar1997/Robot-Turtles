89dfee3dbd1ae716d9a72005e18046e60375acdb
Mecar1997 <56541507+Mecar1997@users.noreply.github.com>
2020-01-06 11:56:12 +0100

    Update Obstacle.java

--- Robot_Turtle/src/Case/Obstacle.java
+++ Robot_Turtle/src/Case/Obstacle.java
@@ -1,5 +1,24 @@
 package Case;
 
 public class Obstacle {
-
+	int type; // 0 = mur de pierre, 1 = mur de glace, 2 = caisse
+	int[] position;
+	
+	public int getType() {
+		return type;
+	}
+	
+	public void setType(int a) {
+		type = a;
+	}
+	
+	public void removeObstacle() {
+		position[0] = -1;
+		position [1] = -1;
+	}
+	
+	public void placeObstacle (int a, int b) {
+		position[0] = a;
+		position[1] = b;
+	}
 }
