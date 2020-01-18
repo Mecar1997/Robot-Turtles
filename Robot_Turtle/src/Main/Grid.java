2e77fa8fe0acdf9b599315d362ec41b13a5875e9
Mecar <msaknisabri@gmail.com>
2020-01-18 14:51:00 +0100

    Update

--- Robot_Turtle/src/Main/Grid.java
+++ Robot_Turtle/src/Main/Grid.java
@@ -1,45 +1,41 @@
 package Main;
 
 public class Grid {
-	int[] type; // 0 = case vide, 1 = mur, 2 = joyau, 3 = tortue
-				// Pour les murs, 0 = mur de pierre, 1 = mur de glace, 2 = caisse
-				// Pour les joyaux, 0 = joyau bleu, 1 = joyau rouge, 2 = joyau vert, 3 = joyau rose
+	int type = 0; // 0 = case vide
+				// Pour les murs, 1 = mur de pierre, 2 = mur de glace, 3 = caisse
+				// Pour les joyaux, 4 = joyau bleu, 5 = joyau rouge, 6 = joyau vert, 7 = joyau rose
+				// 8 = Tortue
 	
 	
 	public Grid() {
 		
 	}
 	
-	public int[] getType() {
+	public int getType() {
 		return type;
 	}
 	
-	public void setType(int a, int b) {
-		type[0] = a;
-		type[0] = b;
+	public void setType(int a) {
+		type = a;
 	}
 	
 	public String toString() { // retourner le nom de le case en fonction de son type
-		if (this.type[0] == 0) {
+		if (this.type == 0) {
 			return ("Case Vide");
-		} else if (this.type[0] == 1) {
-			if (this.type[1] == 0) {
-				return ("Mur de pierre");
-			} else if (this.type[1] == 1) {
-				return ("Mur de glace");
-			} else {
-				return ("Caisse");
-			}
-		} else if (this.type[0] == 2) {
-			if (this.type[1] == 0) {
-				return ("Joyau Bleu");
-			} else if (this.type[1] == 1) {
-				return ("Joyau Rouge");
-			} else if (this.type[1] == 2) {
-				return ("Joyau Vert");
-			} else {
-				return ("Joyau Rose");
-			}
+		} else if (this.type == 1) {
+			return ("Mur de pierre");
+		} else if (this.type == 2) {
+			return ("Mur de glace");
+		} else if (this.type == 3) {
+			return ("Caisse");
+		} else if (this.type == 4) {
+			return ("Joyau Bleu");
+		} else if (this.type == 5) {
+			return ("Joyau Rouge");
+		} else if (this.type == 6) {
+			return ("Joyau Vert");
+		} else if (this.type == 7){
+			return ("Joyau Rose");
 		} else {
 			return ("Tortue");
 		}
--- Robot_Turtle/src/Main/Player.java
+++ Robot_Turtle/src/Main/Player.java
@@ -1,28 +1,47 @@
 package Main;
 
+import java.util.Random;
 
 public class Player {
-	String name;
-	int[] position;
-	int[] startPosition;
+	static int[] position = {0,0};
+	static int[] startPosition = {0,0};
 	int number;
-	int color;
-	int direction; //0 = nord, 1 = Ouest, 2 = Sud, 3 = Est
+	static int color; // 0 = bleu, 1 = rouge, 2 = green, 3 = purple
+	int direction = 2; //0 = nord, 1 = Ouest, 2 = Sud, 3 = Est
 	Card[] program; // Cartes que le joueur a joué
 	Card[] hand; // Cartes que possède le joueur dans sa main
+	static Card[] deck = new Card[37]; // deck du joueur
 	
-	public Player() {
-		// name = a;
-		number = Main.nombreJoueurs + 1;
+	public Player(int c) {
+        color = c;
 	}
 	
 	
-	public String getName() {
-		return name;
+	public static String getName() {
+		if (color == 0) {
+			return "Beep";
+		} else if (color == 1) {
+			return "Pi";
+		} else if (color == 2) {
+			return "Pangle";
+		} else {
+			return "Dot";
+		}
 	}
 	
 	public int[] getPosition() {
 		return position;
+	}
+	
+	public static void setStartPosition(int a, int b) {
+		startPosition[0] = a;
+		startPosition[0] = b;
+		setPosition(a,b);	
+	}
+	
+	public static void setPosition(int a, int b) {
+		position[0] = a;
+		position[0] = b;
 	}
 	
 	public void turnLeft() {
@@ -79,6 +98,64 @@ public class Player {
 
 	public void ditchHand(int a) {
 
+	}
+	
+	public static void shuffleDeck() {
+		System.out.println("DEBUG: Mélange du deck du joueur " + getName());
+        for (int i = 0; i < 37; i++) {
+        	deck[i] = new Card();
+        }
+        Random rand = new Random();
+		int a = 3; //nombre total de cartes lasers dans le jeu
+		do {
+			int i = rand.nextInt(36);
+			if (deck[i].getType() == 0) {
+	        	deck[i].setType(3);
+	        	a = a - 1;
+			}
+		} while (a != 0);
+		a = 8; // nombre total de cartes violettes
+		do {
+			int i = rand.nextInt(36);
+			if (deck[i].getType() == 0) {
+	        	deck[i].setType(2);
+	        	a = a - 1;
+			}
+		} while (a != 0);
+		a = 8; // nombre total de cartes jaunes
+		do {
+			int i = rand.nextInt(36);
+			if (deck[i].getType() == 0) {
+	        	deck[i].setType(1);
+	        	a = a - 1;
+			}
+		} while (a != 0);
+	showDeck();
+	}
+	
+	public static void showDeck() {
+		int j = 0;
+		int v = 0;
+		int l = 0;
+		int b = 0;
+		System.out.print("DEBUG: ");
+        for (int i = 0; i < 37; i++) {
+        	if (deck[i].getType() == 0) {
+        		b++;
+        		System.out.print("B");
+        	} else if (deck[i].getType() == 1) {
+        		j++;
+        		System.out.print("J");
+        	} else if (deck[i].getType() == 2) {
+        		v++;
+        		System.out.print("V");
+        	} else if (deck[i].getType() == 3) {
+        		l++;
+        		System.out.print("L");
+        	}
+        }
+		System.out.println("");
+		System.out.println("DEBUG: Le deck du joueur " + getName() + " contient " + b + " cartes Bleues, " + j + " cartes Jaunes, " + j + " cartes Violettes, " + l + " cartes Laser.");
 	}
 	
 }
